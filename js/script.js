2b1b6ce8f4a5677c1cf7676498459661adf20b92
Adriel Z�rate <adriel_zarate@hotmail.com>
2013-11-12 21:17:02 -0300

    pantallas y agregados varios

--- js/script.js
+++ js/script.js
@@ -2,4 +2,28 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 	  	interval: 3000
 	});
+
+	function scroll(){
+		if($(window).scrollTop() === 0) {
+			$(".navbar").css({height: "98px"});
+			$(".navbar-brand.logo-nav img").css({height: "58px"});
+			$(".nav").css({marginTop: "28px"});
+			return false;
+		}else{
+			$(".navbar").css({height: "50px"});
+			$(".navbar-brand.logo-nav img").css({height: "30px"});
+			$(".nav").css({marginTop: "0px"});
+			return false;
+		}
+	}
+
+	if( $(window).width() > 768) {
+
+		scroll();
+		$(window).scroll(function(){
+			scroll();
+		});
+
+	}
+
 });
